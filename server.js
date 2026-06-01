a29af87e65a518ccd121f8743d836b0ec082bb30
TanvirYestehad0096 <yestehadtanviruiu0096@gmail.com>
2026-06-01 11:23:48 +0600

    fix CORS for live server

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(cors({
   origin: [
     'https://tanviryestehad0096.github.io',
     'http://localhost:3000',
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
   ],
   methods:     ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
